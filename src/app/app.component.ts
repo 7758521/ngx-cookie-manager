60ab7cd8d119d473ac46f6b4f1718779e98496ab
lasantha <lasanthaslakmal@gmail.com>
2017-10-31 21:06:44 +0530

    write cookie service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
 
+import { CookieService } from './lib';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  constructor(private cookieService : CookieService){ }
+
+  add(){
+    this.cookieService.set("", "");
+  }
+
+  get(){
+    this.cookieService.get("");
+  }
+
+  getAll(){ 
+    this.cookieService.getAll();
+  }
+
+  delete(){
+    this.cookieService.delete("");
+  }
+
+  deleteAll(){
+    this.cookieService.deleteAll();
+  }
+
+  check(){
+    this.cookieService.check(""); 
+  }
 }
